af6fdd002038d639c354dd6c1fb267de0094525f
noornee <kendrickbon11@gmail.com>
2022-05-31 19:21:12 +0100

    0x01

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+for i in range (0,100):
+    if len(str(i)) == 1:
+        print(f"0{i}, ", end="")
+    elif i != 99:
+        print(f"{i}, ", end="")
+    else:
+        print(i)
+
+
+
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,7 @@
+0-positive_or_negative.py
+1-last_digit.py
+2-print_alphabet.py
+3-print_alphabt.py
+4-print_hexa.py
+5-print_comb2.py
+README.md
