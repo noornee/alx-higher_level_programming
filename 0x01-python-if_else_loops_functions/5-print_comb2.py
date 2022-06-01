205c8fccdca3b78ce1790c1193d687da1f3b6f65
noornee <kendrickbon11@gmail.com>
2022-06-01 22:24:50 +0100

    classic whitespace prob

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for i in range (100):
+for i in range(100):
     if i != 99:
-        print("{}".format(str(i).zfill(2)), end=",")
+        print("{}".format(str(i).zfill(2)), end=", ")
     else:
         print("{}".format(i))
