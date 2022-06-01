3d8390529578d6ac94113b8059cba8887995371d
noornee <kendrickbon11@gmail.com>
2022-06-01 22:21:32 +0100

    revamp

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-for i in range (0,100):
-    if len(str(i)) == 1:
-        print("0{}, ".format(i), end="")
-    elif i != 99:
-        print("{}, ".format(i), end="")
+
+for i in range (100):
+    if i != 99:
+        print("{}".format(str(i).zfill(2)), end=",")
     else:
-        print(i)
-
-
-
+        print("{}".format(i))
