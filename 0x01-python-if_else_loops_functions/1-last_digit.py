92ef8cb9d3462754f0a13660c4af23a9a1ff47cb
noornee <kendrickbon11@gmail.com>
2022-06-01 20:44:42 +0100

    added blank line after func declaration

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,6 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = str(number)[-1]
 
+
 def positive():
     if int(last_digit) > 5:
         print(f"Last digit of {number} is {last_digit} and is greater than 5")
