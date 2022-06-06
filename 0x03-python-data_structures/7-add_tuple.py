1860fbbcb1b0529219ef8979f08b4ee6860ee8a8
noornee <kendrickbon11@gmail.com>
2022-06-06 22:35:00 +0100

    wip

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,14 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    sum = ()
+    if len(tuple_b) < 2:
+        tuple_b = (*tuple_b, 0)
+    if len(tuple_a) < 2:
+        tuple_a = (*tuple_a, 0)
+    for a, b in zip(tuple_a, tuple_b):
+        sum = (*sum, a + b)
+    return sum
+ 
+tuple_a = (1, 89)
+# print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
+
