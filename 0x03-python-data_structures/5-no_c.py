99cc813dc3ee8a63afed436862a40d9a17766d1d
noornee <kendrickbon11@gmail.com>
2022-06-06 22:36:21 +0100

    nggyu

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+def no_c(my_string):
+    str = ""
+    for i in my_string:
+        if i != "C" and i != "c":
+            str += i
+    return str
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        max_num = 0
+        for i in my_list:
+            if i > max_num:
+                max_num = i
+        return max_num
