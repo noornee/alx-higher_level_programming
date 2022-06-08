deb45afdb3480d246c4fc46ffb0e182b4ffe0e55
noornee <kendrickbon11@gmail.com>
2022-06-09 00:59:56 +0100

    fixes

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max_num = 0
     if a_dictionary is None:
         return None
-    for _, v in a_dictionary.items():
-        if v > max_num:
-            max_num = v
-    return ''.join([k for k, v in a_dictionary.items() if v == max_num])
+    return max(a_dictionary, key=a_dictionary.get)
--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new_matrix = []
-    for i in range(len(matrix)):
-        root = map(lambda n: n ** 2, matrix[i])
-        new_matrix.append(list(root))
-    return new_matrix
+    return (list(map(lambda m: list(map(lambda n: n**2, m)), matrix)))
