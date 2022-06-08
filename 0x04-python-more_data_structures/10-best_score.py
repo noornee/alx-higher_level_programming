f754eb405cefa74f16527470cffad236795aec13
noornee <kendrickbon11@gmail.com>
2022-06-09 00:51:26 +0100

    pycodestyle

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     max_num = 0
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     for _, v in a_dictionary.items():
         if v > max_num:
