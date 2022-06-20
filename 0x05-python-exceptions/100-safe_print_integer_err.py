f9464f43cae79a12c84b99bb99aa086333d190c4
noornee <kendrickbon11@gmail.com>
2022-06-21 00:30:42 +0100

    balls

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as e:
+        sys.stderr.write("Exception: {}".format(e))
+        return False
+    else:
+        return True
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    try:
+        print(message)
+    except NameError:
+        raise NameError
