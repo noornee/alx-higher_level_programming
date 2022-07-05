6bfbf7503dd3d017f0a4056661b56ff0a1c15242
noornee <kendrickbon11@gmail.com>
2022-07-05 20:55:44 +0100

    docstrings

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+""" Module that contains a function that reads
+    from a file and print to stdout
+"""
+
+
 def read_file(filename=""):
     """ function that reads a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename: filename
+
+    Raises:
+        Exception: when file cant be opened
     """
+
     with open(filename, encoding="utf-8") as file:
         print(file.read(), end='')
