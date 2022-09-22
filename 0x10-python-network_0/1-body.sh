0592ed3105e863ad0601dd4ec45f2aaa73a4ebd4
noornee <kendrickbon11@gmail.com>
2022-09-22 21:44:20 +0100

    umm

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # prints the body of a response with status code 200
-status=$(curl --no-progress-meter --head "$1" | grep "HTTP" | cut -d " " -f2)
+status=$(curl -s --head "$1" | grep "HTTP" | cut -d " " -f2)
 if [[ $status == "200" ]];then curl "$1"; fi
