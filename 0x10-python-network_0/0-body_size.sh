d7084d4afbff004d829370bdee49e0fcc696a247
noornee <kendrickbon11@gmail.com>
2022-09-22 21:37:55 +0100

    fix?

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # prints the content-length of a url to stdout
-curl --no-progress-meter --head "$1" | grep -i content-length | cut -d ":" -f2
+curl -sI "$1" | grep -i content-length | cut -d ":" -f2
