4766ce8ccd6732e52d2ecd57d790833f7a0271e1
noornee <kendrickbon11@gmail.com>
2022-09-23 22:32:32 +0100

    function loop through

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""takes in a URL, sends a request to the URL and displays the value of the X-Request-Id"""
+import sys
+from urllib.request import urlopen
+import pprint as p
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urlopen(url) as response:
+        print(response.info())
+        for data in response.getheaders():
+            if data[0] == "X-Request-Id":
+                print(data[1])
