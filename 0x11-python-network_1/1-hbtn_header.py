2b98e3edbe1d2950c2a6c2e469b229e179b19d3b
noornee <kendrickbon11@gmail.com>
2022-09-23 22:33:20 +0100

    removed unused import

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,8 +2,6 @@
 """takes in a URL, sends a request to the URL and displays the value of the X-Request-Id"""
 import sys
 from urllib.request import urlopen
-import pprint as p
-
 
 if __name__ == "__main__":
     url = sys.argv[1]
