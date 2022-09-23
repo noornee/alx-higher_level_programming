577a8cbbcf04f48ff67482ff9fab2ca8758cf3b6
noornee <kendrickbon11@gmail.com>
2022-09-23 22:17:41 +0100

    currently listening to polarize by twenty-one pilots

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""fetches data from https://alx-intranet.hbtn.io/status"""
+from urllib.request import urlopen
+
+
+if __name__ == "__main__":
+    with urlopen("https://alx-intranet.hbtn.io/status") as response:
+        html = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf-8')}")
